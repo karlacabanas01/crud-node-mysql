7fc6c851ecbfbc00fd43fd6ffa3573ba4f460da3
Jkranio <juancarlos@kranio.io>
2025-02-28 03:31:40 -0300

    Update middleware and db config

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,25 +1,25 @@
 const jwt = require("jsonwebtoken");
 
 const authenticateUser = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
+  const authHeader = req.headers.authorization;
 
-  if (!token) {
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res
       .status(401)
       .json({ error: "Acceso no autorizado - Token no proporcionado" });
   }
+  const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(token, "tu_secreto_jwt");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || "tu_secreto_jwt");
     console.log("🔍 Usuario decodificado:", decoded); // 👀 Verifica si se obtiene el user_id
-    req.user = decoded; // Asigna el usuario al request
+    
 
-    if (!req.user.id) {
-      return res
-        .status(403)
-        .json({ error: "Token inválido - No contiene user_id" });
+    if (!decoded.id) {
+      return res.status(403).json({ error: "Token inválido - No contiene user_id" });
     }
-
+    req.user = decoded; // Asigna el usuario al request
+    
     next();
   } catch (error) {
     console.error("Error en autenticación:", error);
